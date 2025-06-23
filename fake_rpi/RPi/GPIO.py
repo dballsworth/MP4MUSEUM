95302a9e3d6c4c4996098b284ff21e80b05503f7
Eric Dalessio <dickensandballsworth@gmail.com>
2025-06-22 20:02:55 -0400

    Add fake_rpi GPIO mock for non-Pi development

---
+++ fake_rpi/RPi/GPIO.py
@@ -0,0 +1,11 @@
+BCM = BOARD = IN = OUT = PUD_UP = PUD_DOWN = HIGH = LOW = RISING = FALLING = BOTH = None
+
+def setmode(*args, **kwargs): pass
+def setup(*args, **kwargs): pass
+def input(*args, **kwargs): return False
+def output(*args, **kwargs): pass
+def cleanup(*args, **kwargs): pass
+def add_event_detect(*args, **kwargs): pass
+def remove_event_detect(*args, **kwargs): pass
+def add_event_callback(*args, **kwargs): pass
+def setwarnings(flag): pass
